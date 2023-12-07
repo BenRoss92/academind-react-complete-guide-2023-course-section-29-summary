4e77f62eea1a8a5d6ef13a21a0aee3889c3b4496
Ben Ross <benjamin.ross@youview.com>
2023-12-07 17:57:24 +0000

    Submitting a post creates a new post list item
    - I skipped over the smaller aim detailed in Lesson 633, 'Preparing the App For State Management'
    - Didn't check the solution in the course - figured out a solution myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { NewPost } from './components/NewPost';
 import { PostList } from './components/PostList';
 import { useState } from 'react';
-
+/**
+ * 
+ * Aim: Create a new post by typing in a author and a body into the form. When you click 'Create post', a new post is displayed.
+ * 
+ * Keep an array of post data at the top level (inside the App component). Have a callback that is defined in the App component, which adds a new post to the array of posts. And then pass that into the NewPost component to pass new data and create new data in the array living higher up. i.e. Define the post array as React state in the App component, then pass the updater function for the posts into the NewPost component and call when clicking the 'create post' button. The array of post data is then passed into the PostList component, then mapped over and each array item could be passed into a Post component to create a Post component for each array item. When the list of posts data changes, the PostList will be automatically re-rendered with the newer list, as the posts data props value will have changed since the last render of the PostList component.
+ * 
+ */
 function App() {
 
   const [postsData, setPostsData] = useState([
@@ -13,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <NewPost/>
+      <NewPost setPostsData={setPostsData}/>
       <PostList postsData={postsData}/>
     </main>
   );
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,43 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-export function NewPost() {
+export function NewPost(props) {
+
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+
+  function handleAuthorChange(event) {
+    setAuthor(event.target.value);
+  }
+
+  function handleBodyChange(event) {
+    setBody(event.target.value);
+  }
+
+  /**
+   * 
+   * Track the author field
+   * - Store whatever value it is currently in React's local state
+   * Use React's useState hook to be able to update the state of the author and get the current state of the author and set the beginning state (empty value)
+   */
+  function createPost(e) {
+    // Log out the author and text values
+    e.preventDefault();
+
+    props.setPostsData(prevPostsData => [...prevPostsData, {author: author, body: body}]);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={createPost}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} value={body} onChange={handleBodyChange}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required value={author} onChange={handleAuthorChange}/>
       </p>
+      <input type="submit" value="Create post"/>
     </form>
   );
 }
