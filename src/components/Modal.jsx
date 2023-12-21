1651a48f1cd1e4d9e3048cbec3d9cac0bbd83c44
Ben Ross <benjamin.ross@youview.com>
2023-12-21 16:02:44 +0000

    Lesson 638: Modal dialog always open, can be closed
    - Close dialog by clicking anywhere on the backdrop
    - Did practically the same thing as the solution in the lesson, just with a slightly different structure

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import classes from './Modal.module.css';
 
 export function Modal({ children }) {
+
+  const [ isOpen, setIsOpen ] = useState(true);
+
   return (
     <>
-      <div className={classes.backdrop} />
-      <dialog className={classes.modal} open>
+      {isOpen && 
+        <div className={classes.backdrop} onClick={() => setIsOpen(false)} />
+      }
+      <dialog className={classes.modal} open={isOpen}>
         {children}
       </dialog>
     </>
