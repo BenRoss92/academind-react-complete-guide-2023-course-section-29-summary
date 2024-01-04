40044469a7d8c861bf10b111afc1ac7c1692f248
Ben Ross <benjamin.ross@youview.com>
2024-01-04 12:18:09 +0000

    Display message if there are no posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -16,19 +16,25 @@ export function PostList({isModalOpen, onCancelPost}) {
           </Modal>
       }
 
-      <ul className={classes.posts}>
-        {postsData.map((postData) => 
-          // Am using body as a crude way of making each React `key` for each Post unique
-          // (and hopefully each body is unique).
-          // React warns you if you are rendering a list of items by mapping over them 
-          // without specifying a truly unique key for each item.
-          // This is because React suggests creating a unique ID yourself for each item
-          // to avoid items being displayed in the wrong order if 
-          // e.g. your array gets accidentally reordered.
-          // If you don't specify a key, React will by default use the index of each item as a key
-          // (as a best guess).
-          <Post key={postData.body} author={postData.author} body={postData.body}/> )}
-      </ul>
+      {postsData.length === 0 && 
+        <h2>No posts</h2>
+      }
+
+      {postsData.length > 0 &&
+        <ul className={classes.posts}>
+          {postsData.map((postData) => 
+            // Am using body as a crude way of making each React `key` for each Post unique
+            // (and hopefully each body is unique).
+            // React warns you if you are rendering a list of items by mapping over them 
+            // without specifying a truly unique key for each item.
+            // This is because React suggests creating a unique ID yourself for each item
+            // to avoid items being displayed in the wrong order if 
+            // e.g. your array gets accidentally reordered.
+            // If you don't specify a key, React will by default use the index of each item as a key
+            // (as a best guess).
+            <Post key={postData.body} author={postData.author} body={postData.body}/> )}
+        </ul>
+      }
     </>
   );
 }
