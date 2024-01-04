7e7433806fc8685991daef9062f883f22b4ef82f
Ben Ross <benjamin.ross@youview.com>
2024-01-04 16:57:52 +0000

    Create posts in the backend + use backend post IDs in UI

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -14,10 +14,19 @@ export function PostList({isModalOpen, onCancelPost}) {
    * After fetching the backend posts and updating the local react state, display whatever posts are in the local react state
    * 
    * Use a useEffect hook to fetch the posts in the background
-   * Pass an empty array as a second argument to useEffect to only make the fetching happen once on the first render and not on re-renders
+   * Pass an empty array as a second argument to useEffect to only make the fetching happen once on the first render and not on re-renders.
+   * By specifying an empty dependency array, we tell React that this useEffect callback doesn't depend on any other values in our code changing.
+   * Therefore it will only ever get executed once, when the component first renders (immediately after the component renders). 
    * 
    * Inside of the useEffect hook, set the postsData state with the updated posts using setPostsData
    * Outside of the useEffect hook, in the return statement (i.e. the render function), reference the new postsData state variable to display the updated posts
+   * 
+   * 'useEffect' can be thought of as a nickname/shortening of 'use side-effect'
+   * A side-effect can be thought of as something that doesn't directly affect anything that will be displayed/rendered to the user
+   * 
+   * We need to use useEffect hook to do this HTTP request, otherwise if we did it outside of this hook, we would cause an infinite loop.
+   * This is because we would perform a fetch, update the state of the component, and then the component would re-render because the state 
+   * has been updated. Which would then cause another fetch, again updating the state and causing an infinite loop.
    */
   useEffect(() => {
     fetch("http://localhost:8080/posts")
@@ -44,8 +53,7 @@ export function PostList({isModalOpen, onCancelPost}) {
       {postsData.length > 0 &&
         <ul className={classes.posts}>
           {postsData.map((postData) => 
-            // Am using body as a crude way of making each React `key` for each Post unique
-            // (and hopefully each body is unique).
+            // Am using the ID that we get from the backend as a way of making each React `key` for each Post unique
             // React warns you if you are rendering a list of items by mapping over them 
             // without specifying a truly unique key for each item.
             // This is because React suggests creating a unique ID yourself for each item
@@ -53,7 +61,7 @@ export function PostList({isModalOpen, onCancelPost}) {
             // e.g. your array gets accidentally reordered.
             // If you don't specify a key, React will by default use the index of each item as a key
             // (as a best guess).
-            <Post key={postData.body} author={postData.author} body={postData.body}/> )}
+            <Post key={postData.id} author={postData.author} body={postData.body}/> )}
         </ul>
       }
     </>
