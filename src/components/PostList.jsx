4747250828ca5b66da1027c4cba67a5cf49a55f3
Ben Ross <benjamin.ross@youview.com>
2024-01-04 11:49:45 +0000

    Remove hardcoded posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,12 +5,7 @@ import { Post } from "./Post";
 import classes from "./PostList.module.css";
 
 export function PostList({isModalOpen, onCancelPost}) {
-  const [postsData, setPostsData] = useState([
-    { author: 'Ben', body: 'I liked this course' },
-    { author: 'Sammy', body: 'I loved this course' },
-    { author: 'Matty', body: 'I disliked this course' },
-    { author: 'Pip', body: 'I hated this course' }
-  ]);
+  const [postsData, setPostsData] = useState([]);
 
   const postComponents = postsData.map(function (postData, index) {
     return <Post key={index} author={postData.author} body={postData.body}/>
