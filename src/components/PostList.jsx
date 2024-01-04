f8ec933fd62fbd1db416f9754fa2e9f6d781d5fd
Ben Ross <benjamin.ross@youview.com>
2024-01-04 12:06:10 +0000

    Explain why React wants a key when mapping over a list of items to render

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -17,8 +17,17 @@ export function PostList({isModalOpen, onCancelPost}) {
       }
 
       <ul className={classes.posts}>
-        {postsData.map((postData, index) => 
-          <Post key={index} author={postData.author} body={postData.body}/> )}
+        {postsData.map((postData) => 
+          // Am using body as a crude way of making each React `key` for each Post unique
+          // (and hopefully each body is unique).
+          // React warns you if you are rendering a list of items by mapping over them 
+          // without specifying a truly unique key for each item.
+          // This is because React suggests creating a unique ID yourself for each item
+          // to avoid items being displayed in the wrong order if 
+          // e.g. your array gets accidentally reordered.
+          // If you don't specify a key, React will by default use the index of each item as a key
+          // (as a best guess).
+          <Post key={postData.body} author={postData.author} body={postData.body}/> )}
       </ul>
     </>
   );
